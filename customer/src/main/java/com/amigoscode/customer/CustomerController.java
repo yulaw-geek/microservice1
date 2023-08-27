cb5d4b2931a7a2c7513a4f9263ab06147f0ccb04
yulaw <yulaw7827@gmail.com>
2023-08-27 20:30:48 +0800

    microservice1

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerController.java
@@ -0,0 +1,24 @@
+package com.amigoscode.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+
+    private CustomerService customerService;
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+        log.info("new customer registration {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
+    }
+}
